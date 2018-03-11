e86dbb786c20da49ecf55416cdd92b33fc15cafb
David Callizaya <davidcallizaya@gmail.com>
2018-03-11 12:09:23 -0400

    remove debug

--- index.php
+++ index.php
@@ -15,9 +15,6 @@ if (!$token) {
 echo '<pre>';
 echo "[connected]\n";
 
-echo sprintf('<a href="%s" download="%s">%s</a>', 'lca201702.issue.mobi', 'lca201702.issue.mobi', 'lca201702.issue.mobi'),"\n";
-echo sprintf('<a href="%s" download>%s</a>', 'lca201702.issue.mobi', 'lca201702.issue.mobi'),"\n";
-echo sprintf('<a href="%s" target="_blank">%s</a>', 'lca201702.issue.mobi', 'lca201702.issue.mobi'),"\n";
 foreach($drive->listInPath('kindle') as $file) {
     echo sprintf('<a href="%s" download="%s">%s</a>', 'download.php?id=' . $file->id, $file->name, $file->name),"\n";
 }
